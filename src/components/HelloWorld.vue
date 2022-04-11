89e3a547cbca9f9dd1a142ee099a51e26c1a91fe
minicherry <cherrychengyufen@163.com>
2022-04-11 10:03:20 +0800

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { ref } from 'vue'
+// import * as THREE from 'three' //引入Threejs
+import { defineProps } from 'vue'
+defineProps({
+  msg: String
+})
+const count = ref(0)
+const init = () => {
+  console.log(this, '---')
+
+  // const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
+
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+
+  const geometry = new THREE.BoxGeometry()
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  const cube = new THREE.Mesh(geometry, material)
+  // scene.add(cube)
+
+  camera.position.z = 5
+
+  function animate() {
+    requestAnimationFrame(animate)
+
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+
+    // renderer.render(scene, camera)
+  }
+  animate()
+}
+init()
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
