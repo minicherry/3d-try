75f6d49a675638114406e4de552b6809faebaa8f
minicherry <cherrychengyufen@163.com>
2022-04-12 11:42:42 +0800

    物体拉伸

--- src/pages/scale.vue
+++ src/pages/scale.vue
@@ -1,7 +1,11 @@
 <script setup>
 import { ref } from 'vue'
 import * as THREE from 'three' //引入Threejs
-const count = ref(0)
+let scaleX = ref(1)
+let scaleY = ref(1)
+let scaleZ = ref(1)
+
+let cube = {}
 const init = () => {
   const scene = new THREE.Scene()
   const camera = new THREE.PerspectiveCamera(
@@ -17,24 +21,40 @@ const init = () => {
 
   const geometry = new THREE.BoxGeometry()
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-  const cube = new THREE.Mesh(geometry, material)
+  cube = new THREE.Mesh(geometry, material)
   scene.add(cube)
 
   camera.position.z = 5
 
   function animate() {
     requestAnimationFrame(animate)
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
     renderer.render(scene, camera)
   }
   animate()
 }
 init()
+// 输入x，y，z拉伸
+const changeX = param => {
+  scaleX.value = param
+  cube.scale.set(param,scaleY.value,scaleZ.value)
+}
+const changeY = param => {
+  scaleY.value = param
+  cube.scale.set(scaleX.value,param,scaleZ.value)
+}
+const changeZ = param => {
+  scaleZ.value = param
+  cube.scale.set(scaleX.value,scaleY.value,param)
+}
+
 </script>
 
 <template>
-<input type="text"  @change="changeX" v-model.trim.lazy="scaleX">
-<input type="text"  @change="changeY" v-model.trim.lazy="scaleY">
-<input type="text"  @change="changeZ" v-model.trim.lazy="scaleZ">
+<input type="text"  @change="changeX(scaleX)" v-model.trim.lazy="scaleX">
+<input type="text"  @change="changeY(scaleY)" v-model.trim.lazy="scaleY">
+<input type="text"  @change="changeZ(scaleZ)" v-model.trim.lazy="scaleZ">
 
 </template>
 
