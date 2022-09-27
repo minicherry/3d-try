6559725757be486627bfeb2e2927932de4ba4edc
minicherry <cherrychengyufen@163.com>
2022-09-27 20:19:45 +0800

    下雪或下雨粒子系统

--- src/pages/weather.vue
+++ src/pages/weather.vue
@@ -64,6 +64,19 @@ export default {
     const animate = () =>{
       const delta = clock.getDelta();
       controls.update(delta);
+      // scene.children.forEach(function (child) {
+      //   if (child instanceof THREE.Points) {
+      //     var vertices = child.geometry.vertices;
+      //     vertices.forEach(function (v) {
+      //       v.y = v.y - (v.velocityY);
+      //       v.x = v.x - (v.velocityX);
+      //       if (v.y <= -20) v.y = 60;
+      //       if (v.x <= -20 || v.x >= 20) v.velocityX = v.velocityX * -1;
+      //     });
+      //     // 顶点变动之后需要更新，否则无法实现特效
+      //     child.geometry.verticesNeedUpdate = true
+      //   }
+      // })
       requestAnimationFrame(animate);
       renderer.render(scene, camera);
     }
@@ -74,8 +87,62 @@ export default {
       initOrbit()
       animate()
     }
+    const createSnowClouds =(size, transparent, opacity, sizeAttenuation, color) =>{
+      let snowflake1 = new URL('../assets/weather/points/snowflake1.png', import.meta.url).href
+      let snowflake2 = new URL('../assets/weather/points/snowflake2.png', import.meta.url).href
+      let snowflake3 = new URL('../assets/weather/points/snowflake3.png', import.meta.url).href
+      let snowflake4 = new URL('../assets/weather/points/snowflake4.png', import.meta.url).href
+      let snowflake5 = new URL('../assets/weather/points/snowflake5.png', import.meta.url).href
+
+      var texture1 = new THREE.TextureLoader().load(snowflake1);
+      var texture2 = new THREE.TextureLoader().load(snowflake2);
+      var texture3 = new THREE.TextureLoader().load(snowflake3);
+      var texture4 = new THREE.TextureLoader().load(snowflake4);
+      var texture5 = new THREE.TextureLoader().load(snowflake5);
+
+      scene.add(createPointCloud("snowflake", texture1, size, transparent, opacity, sizeAttenuation, color));
+      scene.add(createPointCloud("snowflake", texture2, size, transparent, opacity, sizeAttenuation, color));
+      scene.add(createPointCloud("snowflake", texture3, size, transparent, opacity, sizeAttenuation, color));
+      scene.add(createPointCloud("snowflake", texture4, size, transparent, opacity, sizeAttenuation, color));
+      scene.add(createPointCloud("snowflake", texture5, size, transparent, opacity, sizeAttenuation, color));
+      console.log(scene)
+    }
+    const createPointCloud = (name, texture, size, transparent, opacity, sizeAttenuation, color) =>{
+      var geom = new THREE.Geometry();
+      var material = new THREE.PointsMaterial({
+          size: size,
+          transparent: transparent,
+          opacity: opacity,
+          map: texture,
+          blending: THREE.AdditiveBlending,
+          depthWrite: false,
+          sizeAttenuation: sizeAttenuation,
+      });
+
+      var range = 120;
+      for (var i = 0; i < 5000; i++) {
+        var particle = new THREE.Vector3(Math.random() * range - range / 2, Math.random() * range - range / 2, Math.random() * range - range / 2);
+        particle.velocityY = 0.1 + Math.random() / 5;
+        particle.velocityX = (Math.random() - 0.5) / 3;
+        geom.vertices.push(particle);
+      }
+
+      let system = new THREE.Points(geom, material);
+      // system.name = name;
+      // system.sortParticles = true;
+      system.verticesNeedUpdate = true;
+
+      return system;
+    }
+    const createRainClouds = (size, transparent, opacity, sizeAttenuation, color) =>{
+      let raindrop = new URL('../assets/weather/points/raindrop-3.png', import.meta.url).href
+      console.log(raindrop)
+      const texture1 = new THREE.TextureLoader().load(raindrop)
+      scene.add(createPointCloud("system1", texture1, size, transparent, opacity, sizeAttenuation, color));
+    }
     onMounted(() => {
       init()
+      createSnowClouds(controls.size, controls.transparent, controls.opacity, controls.sizeAttenuation, controls.color);
     })
   }
 }
